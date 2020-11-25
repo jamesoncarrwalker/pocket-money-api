c1840c04ca0cfcd3ef12c1ffebbbfc3fe37b1893
jamesskywalker <jamescarr28@hotmail.co.uk>
2020-11-25 20:39:07 +0000

    Unit tests for abstract connection object

---
+++ framework/abstractClass/AbstractConnectionObject.php
@@ -0,0 +1,54 @@
+<?php
+namespace abstractClass;
+use interfaces\QueryConnectionManagerInterface;
+use interfaces\QueryPrepareInterface;
+use interfaces\QueryResultsInterface;
+
+/**
+ * Created by PhpStorm.
+ * User: jamesskywalker
+ * Date: 12/10/2019
+ * Time: 21:11
+ */
+
+/**
+ * Class AbstractConnectionObject
+ * Wrapper class for all the connection interfaces
+ */
+abstract class AbstractConnectionObject implements QueryPrepareInterface, QueryResultsInterface, QueryConnectionManagerInterface {
+    protected $conn;
+    private $connectionVars;
+    protected $host;
+    protected $database;
+    protected $username;
+    protected $password;
+    protected $charset;
+
+    public function __construct(array $connectionVars) {
+        $this->connectionVars = $connectionVars;
+        $this->setConnectionVars();
+    }
+
+    protected function setConnectionVars() {
+        $this->host = $this->connectionVars['HOST'] ?? false;
+        $this->database = $this->connectionVars['NAME'] ?? false;
+        $this->username = $this->connectionVars['USERNAME'] ?? false;
+        $this->password = $this->connectionVars['PASSWORD'] ?? false;
+        $this->charset = $this->connectionVars['CHARSET'] ?? false;
+    }
+
+    public function getConnectionVar(string $var) {
+        if(property_exists($this,$var)) {
+            if($var == 'password' && isset($this->password)) {
+                return '';
+            }
+
+            return $this->{$var};
+        }
+
+        return false;
+    }
+
+
+
+}
