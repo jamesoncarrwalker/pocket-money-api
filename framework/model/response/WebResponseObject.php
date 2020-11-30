a5a62e301f1c1a4248700e357462401671f25374
jamesskywalker <jamescarr28@hotmail.co.uk>
2020-11-30 17:03:52 +0000

    removed add slashes corrupting data in the front end

---
+++ framework/model/response/WebResponseObject.php
@@ -0,0 +1,52 @@
+<?php
+namespace model\response;
+
+
+use abstractClass\AbstractHttpResponseObject;
+use interfaces\TemplateInterface;
+
+
+/**
+ * Created by PhpStorm.
+ * User: jamesskywalker
+ * Date: 12/10/2019
+ * Time: 19:39
+ */
+class WebResponseObject extends AbstractHttpResponseObject {
+
+    private $templateParser;
+    private $templateName;
+
+
+    public function __construct(TemplateInterface $templateParser) {
+        $this->templateParser = $templateParser;
+    }
+
+    public function setResponse() {
+        if (!isset($this->templateName)) {
+            $this->response = json_encode($this->responseData??[]);
+        } else {
+            $this->templateParser->setTemplate($this->templateName, $this->responseData);
+            $this->response = $this->templateParser->parseString();
+        }
+    }
+
+    public function setResponseData(string $name, $data) {
+        if ($name == 'template') {
+            $this->templateName = $data;
+        } else if ($name == 'header') {
+            $this->headers[] = $data;
+        } else {
+            $this->responseData[$name] = $data;
+        }
+
+    }
+
+
+    public function setBasicHeaders() {
+        //implement the headers you need
+        if(!isset($this->templateName)) {
+            $this->headers[] = 'Content-Type: application/json';
+        }
+    }
+}
